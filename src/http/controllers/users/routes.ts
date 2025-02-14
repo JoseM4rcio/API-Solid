da6db8dfe494910e8fc5b1204c4799033065824e
JoseM4rcio <jm4536@outlook.com>
2025-02-14 10:15:40 -0300

    Outros controllers da academia

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -6,7 +6,6 @@ import { verifyJWT } from "../../middlewares/verify-jwt";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
-
   app.post("/sessions", authenticate);
 
   /** Authenticated */
